5cda84d956472c87d36e55ff17ae48e405a29d95
NikitaChernikov <nikitachernikov20006@gmail.com>
2021-03-24 13:11:05 +0600

    Menu added

---
+++ Assets/Scripts/Clouds.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clouds : MonoBehaviour
+{
+
+    public float moveSpeed;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = transform.right * moveSpeed;
+    }
+}
--- Assets/Scripts/RightSide.cs
+++ Assets/Scripts/RightSide.cs
@@ -6,9 +6,9 @@ public class RightSide : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Player")
+        if(other.tag == "Player" || other.tag == "Cloud")
         {
-            other.gameObject.transform.position = new Vector3(-3.11f, other.gameObject.transform.position.y, -.5f);
+            other.gameObject.transform.position = new Vector3(-225, other.gameObject.transform.position.y, -.5f);
         }
     }
 }

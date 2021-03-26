57557303d85f53601ee07d387e532d657a81bd32
NikitaChernikov <nikitachernikov20006@gmail.com>
2021-03-26 13:46:10 +0600

    Nothing Happened

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -48,4 +48,6 @@ public class AudioManager : MonoBehaviour
         }
         s.source.Play();
     }
+
+    
 }
---
+++ Assets/Scripts/MuteToggle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Toggle))]
+public class MuteToggle : MonoBehaviour
+{
+    Toggle myToggle;
+    public Sound sound;
+
+    // Start is called before the first frame update
+    [System.Obsolete]
+    void Start()
+    {
+        
+        myToggle = GetComponent<Toggle>();
+        if (sound.volume == 0)
+        {
+            myToggle.isOn = false;
+        }
+    }
+
+    public void ToggleAudioOnValueChange(bool audioIn)
+    {
+        if(audioIn)
+        {
+            sound.volume = 1f;
+        }
+        else
+        {
+            sound.volume = 0f;
+        }
+    }
+}
